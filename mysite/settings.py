fb49667f82f8f8dc99473272fdfa7b871838c7e3
coherencedemo <164892412+coherencedemo@users.noreply.github.com>
2024-09-10 12:46:27 +0200

    remove load_dotenv()

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,8 +18,6 @@ import dj_database_url
 
 from pathlib import Path
 
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
